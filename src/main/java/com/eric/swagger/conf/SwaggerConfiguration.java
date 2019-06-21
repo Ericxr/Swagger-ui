fa2ed7806e04c6bd6ca9e97c109658a29f8f4815
gaoxin <gaoxin@gloriousmed.com>
2019-06-21 11:32:09 +0800

    test

--- src/main/java/com/eric/swagger/conf/SwaggerConfiguration.java
+++ src/main/java/com/eric/swagger/conf/SwaggerConfiguration.java
@@ -2,6 +2,8 @@ package com.eric.swagger.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -18,10 +20,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebMvcConfigurationSupport{
 
 
-
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars*")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
         @Bean
         public Docket buildDocket(){
             return new Docket(DocumentationType.SWAGGER_2)
