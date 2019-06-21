da5a2e08045878d6c0c3ad8a2aac21022fb995c1
gaoxin <gaoxin@gloriousmed.com>
2019-06-21 11:12:40 +0800

    lllllllll

--- src/main/java/com/eric/swagger/conf/ServletContextConfig.java
+++ src/main/java/com/eric/swagger/conf/ServletContextConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 /**
  * 拦截器配置
+ * 
  * @author zyl
  *
  */
@@ -13,27 +15,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class ServletContextConfig extends WebMvcConfigurationSupport {
 
     /**
-     * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。
-     * 需要重新指定静态资源
+     * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。 需要重新指定静态资源
+     * 
      * @param registry
      */
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        // 将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // swagger2
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
+
     }
-
-
-    /**
-     * 配置servlet处理
-     */
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
 }
